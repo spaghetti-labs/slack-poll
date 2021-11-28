e14d60fdb20eec3865a81e7c4784d45b63c99d39
Muhammet Ali AKBAY <me@muhammetaliakbay.com>
2021-11-28 13:39:21 +0100

    Added kustomize files

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
 import { createConnection, getConnection } from "typeorm";
 import { entities } from "./entities";
+import { PollEntity } from "./entities/poll";
 import { migrations } from "./migrations";
 
 export async function connectDB(
@@ -16,4 +17,8 @@ export async function connectDB(
         migrations,
         migrationsRun: true,
     })
+
+    console.log(`Connected to the DB at the file ${path}.`)
+    const pollCount = await PollEntity.count()
+    console.log(`${pollCount} polls found in the db.`)
 }
